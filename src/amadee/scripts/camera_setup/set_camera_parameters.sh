7a17a1ed41b1706a31b61164d380295fb2770b39
rojung <roland.jung@aau.at>
2020-07-09 18:20:32 +0200

    [TASK] set_camera_parameter() updates

--- src/amadee/scripts/camera_setup/set_camera_parameters.sh
+++ src/amadee/scripts/camera_setup/set_camera_parameters.sh
@@ -14,20 +14,48 @@ cleanup() {
 
 }
 
+DEVICE_IP=192.168.0.158
+USER=core
+export ROS_MASTER_URI=http://${DEVICE_IP}:11311
+
 trap cleanup EXIT TERM
 
 # remove old files
 rm -f yamlfiles/camera_parameters.yaml
 
 echo "--------- WAIT UNTIL THE CAMERA IS READY ----------"
+echo ""
 
 # launch camera
-roslaunch realsense2_camera rs_camera.launch > /dev/null & pids+=( "$!" )
+ssh -f ${USER}@${DEVICE_IP} "bash -c 'source ~/.ros_env.bash && roscore >/dev/null 2>&1 &'"
+until rostopic list >/dev/null 2>&1; do sleep 1; done
+ssh -f ${USER}@${DEVICE_IP} "bash -c 'source ~/.ros_env.bash && roslaunch amadee_bringup ueye_cam.launch >/dev/null 2>&1 &'"
 
 # wait some time to have camera ready
-sleep 15
+sleep 10
 
-rqt --perspective-file perspective/realsense_cam_params.perspective & pids+=( "$!" )
+echo "------------ OPENING CAMERA SETUP GUI -------------"
+echo ""
+
+rqt --perspective-file perspective/ids_cam_params.perspective & pids+=( "$!" )
+
+echo "--------------- PRESS 's' TO SAVE -----------------"
+echo ""
+
+read -n 1 k <&1
+
+echo ""
+
+if [[ $k = s ]] ; then
+
+#echo "[TODO] dumping parameters..."
+
+#rosrun dynamic_reconfigure dynparam dump /camera yamlfiles/camera_parameters.yaml
+
+#printf "\n---------------- PARAMETER SAVED ------------------\n\n"
+fi
+
+echo "-------------- PRESS [CTRL-C] TO EXIT ---------------"
+echo ""
 
 wait
-echo "--------- SETUP CAMERA PARAMETERS DONE ----------"
