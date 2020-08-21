6d6cb1dcfafcf5d5abe7368addc27fa8ae73b7de
Christian Brommer <christian.brommer@aau.at>
2020-08-21 17:02:13 +0200

    feat: updated record scripts and added grouping for full setup

--- src/amadee/amadee_bringup/record_optitrac.sh
+++
@@ -1,7 +0,0 @@
-rosbag record --split --size=500 -o optitrac_pose \
-	/mavros/imu/data_raw /mavros/imu/mag /mavros/imu/static_pressure \
-	/Twins/vrpn_client/raw_transform \       
-	/camera/accel/imu_info /camera/accel/sample /camera/gyro/imu_info /camera/gyro/sample \
-	/camera/odom/sample \
-        /camera/fisheye1/image_raw
-
--- src/amadee/amadee_bringup/record_scripts/record_outdoor_2gps.sh
+++ src/amadee/amadee_bringup/record_scripts/record_outdoor_2gps.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-bag_name="outdoor"
+#Record
+# - two ublox rtk gps position and velocity 
+# - ids camera images
+# - px4 imu, pressure, magnetometer and GPS
+
+bag_name="outdoor_2_gps"
 
 px4_topics=(
 "/mavros/imu/data_raw"
@@ -13,34 +18,21 @@ px4_topics=(
 )
 
 ids_camera_topics=( "/camera/image_raw" )
-real_sense_topics=(
-"/camera/accel/imu_info"
-"/camera/accel/sample"
-"/camera/gyro/imu_info"
-"/camera/gyro/sample"
-"/camera/odom/sample"
-"/camera/fisheye1/camera_info"
-"/camera/fisheye1/image_raw"
-"/camera/fisheye2/camera_info"
-"/camera/fisheye2/image_raw")
 
 rtk_gps_topic=(
-"/rtk/fix"
-"/rtk/fix_velocity"
+"/rtk_gps_1/fix"
+"/rtk_gps_1/fix_velocity"
+"/rtk_gps_2/fix"
+"/rtk_gps_2/fix_velocity"
 )
 
 groups_to_record=(
 ${px4_topics[@]}
 ${ids_camera_topics[@]}
-#${real_sense_topics[@]}
 ${rtk_gps_topic[@]}
 )
 
 topics_to_record=${groups_to_record[@]}
-
-# for i in "${px4_topics[@]}" "${ids_camera_topics[@]}" "${real_sense_topics[@]}" "${rtk_gps_topic[@]}"; do
-# 	topics_to_record+=" $i";
-# done
 
 echo "Bagname: " ${bag_name}
 echo "Topics to record: " ${topics_to_record}
