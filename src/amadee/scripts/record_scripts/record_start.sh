7a8ec31192975b2a215cb977929aaa9c3dcf85b9
Martin Scheiber <martin.scheiber@aau.at>
2021-07-30 10:25:03 +0200

    subm: updated data recorder for killing

--- src/amadee/scripts/record_scripts/record_start.sh
+++ src/amadee/scripts/record_scripts/record_start.sh
@@ -6,7 +6,7 @@ REC_IN="${1}"
 PI2_IP=10.42.0.102
 
 CMD_PI1="/home/core/catkin_ws/src/amadee/scripts/record_scripts/record_full_setup.sh"
-REM_PI2="ssh core@${PI2_IP}"
+REM_PI2="ssh -t -t core@${PI2_IP}"
 CMD_PI2="/home/core/catkin_ws/src/amadee/scripts/record_scripts/record_full_setup.sh"
 # CMD_PI1="/home/core/catkin_ws/src/amadee/scripts/record_scripts/dummy/sleep_dummy.sh"
 # CMD_PI2="/home/core/catkin_ws/amadee20_cws/src/amadee/scripts/record_scripts/dummy/sleep_dummy.sh"
@@ -16,7 +16,7 @@ CMD_PI2="/home/core/catkin_ws/src/amadee/scripts/record_scripts/record_full_setu
 PATH_LOCAL="/home/core/rec_local/"
 PATH_MEDIA="/home/core/rec_media/"
 
-if [ "${REC_IN}" == "full" ] || [ "${REC_IN}" == "cam" ] || [ "${REC_IN}" == "cam" ] ; then
+if [ "${REC_IN}" == "full" ] || [ "${REC_IN}" == "cam" ] || [ "${REC_IN}" == "sensors" ] ; then
   parallel -u ::: \
   "${CMD_PI1} dev1_${REC_IN} ${PATH_LOCAL} ${PATH_MEDIA}" \
   "${REM_PI2} 'source /home/core/.ros_env.bash; ${CMD_PI2} dev2_${REC_IN} ${PATH_LOCAL} ${PATH_MEDIA}'"
