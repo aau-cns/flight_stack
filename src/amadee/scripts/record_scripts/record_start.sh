a41fd7f2558dafe77444e7775bfa64de3f958d74
Martin Scheiber <martin.scheiber@aau.at>
2021-07-30 08:50:39 +0200

    fix: minor fixes for running record

--- src/amadee/scripts/record_scripts/record_start.sh
+++ src/amadee/scripts/record_scripts/record_start.sh
@@ -19,7 +19,7 @@ PATH_MEDIA="/home/core/rec_media/"
 if [ "${REC_IN}" == "full" ] || [ "${REC_IN}" == "cam" ] || [ "${REC_IN}" == "cam" ] ; then
   parallel -u ::: \
   "${CMD_PI1} dev1_${REC_IN} ${PATH_LOCAL} ${PATH_MEDIA}" \
-  "${REM_PI1} '${CMD_PI2} dev2_${REC_IN} ${PATH_LOCAL} ${PATH_MEDIA}'"
+  "${REM_PI2} 'source /home/core/.ros_env.bash; ${CMD_PI2} dev2_${REC_IN} ${PATH_LOCAL} ${PATH_MEDIA}'"
 else
   parallel -u ::: "${CMD_PI1} ${REC_IN}" "$CMD_PI2 ${REC_IN}"
 fi;
