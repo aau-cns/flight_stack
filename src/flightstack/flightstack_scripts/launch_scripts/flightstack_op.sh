e07c147f3a0f6e555defdb64b71d3ccd1f2e344a
Martin Scheiber <martin.scheiber@aau.at>
2022-02-17 15:29:44 +0100

    maint: trajectory update

--- src/flightstack/flightstack_scripts/launch_scripts/flightstack_op.sh
+++ src/flightstack/flightstack_scripts/launch_scripts/flightstack_op.sh
@@ -18,9 +18,13 @@ elif [ "${type}" = "dualpose_gps" ]; then
 
   OPS="sleep 10; roslaunch flightstack_bringup fs_operator.launch dev_id:=1 estimator_init_service_name:='/mars_dualpose_node/init_service' 'config_filepath:=\$(find flightstack_bringup)/configs/autonomy/config_gps.yaml'"
 
-elif [ "${type}" = "vision" ]; then
+elif [ "${type}" = "vision_gps" ]; then
 
   OPS="sleep 10; roslaunch flightstack_bringup fs_operator.launch dev_id:=1 estimator_init_service_name:='/mars_gps_vision_node/init_service' 'config_filepath:=\$(find flightstack_bringup)/configs/autonomy/config_gps.yaml'"
+
+elif [ "${type}" = "vision_rtk" ]; then
+
+  OPS="sleep 10; roslaunch flightstack_bringup fs_operator.launch dev_id:=1 estimator_init_service_name:='/mars_gps_vision_node/init_service' 'config_filepath:=\$(find flightstack_bringup)/configs/autonomy/config_rtk.yaml'"
 
 elif [ "${type}" = "gps" ]; then
 
